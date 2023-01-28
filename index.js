3cc477e75ecd7a77d9279fa92b98759631830062
Zilan Zaman <aazilanzaman@gmail.com>
2023-01-28 20:18:32 +0600

    First commit of CV

---
+++ index.js
@@ -0,0 +1,5 @@
+var date = document.lastModified;
+var locate= window.location.href;
+document.getElementById("LastChange").innerHTML = `The document was last modified on : ${date} <br>
+The pages location : ${locate} `;
+       
